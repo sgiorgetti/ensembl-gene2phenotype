cd392025fd19bff6d85ba69c99b517ae16929061
Anja Thormann <anja@ebi.ac.uk>
2015-10-20 15:24:54 +0100

    add store, update and delete methods

--- modules/Bio/EnsEMBL/G2P/DBSQL/GenomicFeatureDiseaseActionAdaptor.pm
+++ modules/Bio/EnsEMBL/G2P/DBSQL/GenomicFeatureDiseaseActionAdaptor.pm
@@ -19,6 +19,129 @@ use Bio::EnsEMBL::G2P::DBSQL::BaseAdaptor;
 use Bio::EnsEMBL::G2P::GenomicFeatureDiseaseAction;
 our @ISA = ('Bio::EnsEMBL::G2P::DBSQL::BaseAdaptor');
 
+sub store {
+  my $self = shift;
+  my $GFD_action = shift; 
+  my $user = shift;
+  my $dbh = $self->dbc->db_handle;
+
+  if (!ref($GFD_action) || !$GFD_action->isa('Bio::EnsEMBL::G2P::GenomicFeatureDiseaseAction')) {
+    die ('Bio::EnsEMBL::G2P::GenomicFeatureDiseaseAction arg expected');
+  }
+  
+  if (!ref($user) || !$user->isa('Bio::EnsEMBL::G2P::User')) {
+    die ('Bio::EnsEMBL::G2P::User arg expected');
+  }
+ 
+  my $sth = $dbh->prepare(q{
+    INSERT INTO genomic_feature_disease_action (
+      genomic_feature_disease_id,
+      allelic_requirement_attrib,
+      mutation_consequence_attrib
+    ) VALUES (?,?,?)
+  });
+
+  $sth->execute(
+    $GFD_action->genomic_feature_disease_id,
+    $GFD_action->allelic_requirement_attrib,
+    $GFD_action->mutation_consequence_attrib,
+  );
+  $sth->finish();
+
+  my $dbID = $dbh->last_insert_id(undef, undef, 'genomic_feature_disease_action', 'genomic_feature_disease_action_id');
+
+  $GFD_action->{genomic_feature_disease_action_id} = $dbID;
+
+  $self->update_log($GFD_action, $user, 'create');
+
+  return $GFD_action;
+}
+
+sub update {
+  my $self = shift;
+  my $GFD_action = shift;
+  my $user = shift;
+  my $dbh = $self->dbc->db_handle;
+
+  if (!ref($GFD_action) || !$GFD_action->isa('Bio::EnsEMBL::G2P::GenomicFeatureDiseaseAction')) {
+    die ('Bio::EnsEMBL::G2P::GenomicFeatureDiseaseAction arg expected');
+  }
+  
+  if (!ref($user) || !$user->isa('Bio::EnsEMBL::G2P::User')) {
+    die ('Bio::EnsEMBL::G2P::User arg expected');
+  }
+  
+  my $sth = $dbh->prepare(q{
+    UPDATE genomic_feature_disease_action
+      SET genomic_feature_disease_id = ?,
+          allelic_requirement_attrib = ?,
+          mutation_consequence_attrib = ?
+      WHERE genomic_feature_disease_action_id = ?
+  });
+  $sth->execute(
+    $GFD_action->{genomic_feature_disease_id},
+    $GFD_action->allelic_requirement_attrib,
+    $GFD_action->mutation_consequence_attrib,
+    $GFD_action->{genomic_feature_disease_action_id}
+  ); 
+  $sth->finish();
+
+  $self->update_log($GFD_action, $user, 'update');
+
+  return $GFD_action;
+}
+
+sub delete {
+  my $self = shift;
+  my $GFDA = shift; 
+  my $user = shift;
+  my $dbh = $self->dbc->db_handle;
+
+  if (!ref($GFDA) || !$GFDA->isa('Bio::EnsEMBL::G2P::GenomicFeatureDiseaseAction')) {
+    die ('Bio::EnsEMBL::G2P::GenomicFeatureDiseaseAction arg expected');
+  }
+  
+  if (!ref($user) || !$user->isa('Bio::EnsEMBL::G2P::User')) {
+    die ('Bio::EnsEMBL::G2P::User arg expected');
+  }
+
+  $self->update_log($GFDA, $user, 'delete');
+
+  my $sth = $dbh->prepare(q{
+    DELETE FROM genomic_feature_disease_action WHERE genomic_feature_disease_action_id = ?;
+  });
+  
+  $sth->execute($GFDA->dbID);
+  $sth->finish();
+}
+
+sub update_log {
+  my $self = shift;
+  my $GFD_action = shift;
+  my $user = shift;
+  my $action = shift;
+  my $dbh = $self->dbc->db_handle;
+  my $sth = $dbh->prepare(q{
+    INSERT INTO genomic_feature_disease_action_log(
+      genomic_feature_disease_action_id,
+      genomic_feature_disease_id,
+      allelic_requirement_attrib,
+      mutation_consequence_attrib,
+      created,
+      user_id,
+      action 
+    ) VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP, ?, ?)
+  });
+  $sth->execute(
+    $GFD_action->dbID,
+    $GFD_action->genomic_feature_disease_id,
+    $GFD_action->allelic_requirement_attrib || undef,
+    $GFD_action->mutation_consequence_attrib || undef,
+    $user->user_id,
+    $action  
+  ); 
+}
+
 sub fetch_by_dbID {
   my $self = shift;
   my $dbID = shift;
