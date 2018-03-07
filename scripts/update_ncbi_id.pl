ea2b14b71e58ff9f57e2a015e783607feafa7516
Anja Thormann <anja@ebi.ac.uk>
2018-03-07 12:30:26 +0000

    update

--- scripts/update_ncbi_id.pl
+++ scripts/update_ncbi_id.pl
@@ -4,8 +4,8 @@ use warnings;
 use FileHandle;
 use Bio::EnsEMBL::Registry;
 
-my $working_dir = '/hps/nobackup/production/ensembl/anja/G2P/text_mining/';
-my $registry_file = "$working_dir/registry_file_live";
+my $working_dir = '/Users/anja/Documents/G2P/';
+my $registry_file = "$working_dir/ensembl.registry.mar2018";
 my $registry = 'Bio::EnsEMBL::Registry';
 $registry->load_all($registry_file);
 
@@ -13,14 +13,14 @@ my $dbh = $registry->get_DBAdaptor('human', 'gene2phenotype')->dbc->db_handle;
 
 my $hgnc_id_2_ncbi_id_mappings = {};
 
-my $fh = FileHandle->new("$working_dir/hgnc_id_prev_symbol_ncbi_id_mappings_11_2017_mart.txt", 'r');
+my $fh = FileHandle->new("$working_dir/data/hgnc_id_prev_symbol_ncbi_id_mappings_03_2018_mart.txt", 'r');
 
 while (<$fh>) {
   chomp;
   next if (/^HGNC ID/);
   my ($hgnc_id, $approved_symbol, $prev_symbol, $ncbi_id) = split/\t/;
   $hgnc_id =~ s/HGNC://;
-  if ($hgnc_id, $ncbi_id) {
+  if ($hgnc_id && $ncbi_id) {
     $dbh->do(qq{update genomic_feature set ncbi_id=$ncbi_id where hgnc_id=$hgnc_id;}) or die $dbh->errstr;    
   }
 }
