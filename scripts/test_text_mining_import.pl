3ed6ecc29dbdc4587eadb1ae57fd3aa801af4379
Anja Thormann <anja@ebi.ac.uk>
2018-11-14 14:11:39 +0000

    update

--- scripts/test_text_mining_import.pl
+++ scripts/test_text_mining_import.pl
@@ -3,26 +3,21 @@ use warnings;
 
 use Bio::EnsEMBL::Registry;
 
-my $registry_file = '/Users/anja/Documents/G2P/update_94/ensembl.registry.oct2018';
+my $registry_file = '/ensembl.registry.nov2018';
 my $registry = 'Bio::EnsEMBL::Registry';
 $registry->load_all($registry_file);
 my $dbh = $registry->get_DBAdaptor('human', 'gene2phenotype')->dbc->db_handle;
 
 my $g2p_pmids = get_pmids_linked_to_GFDs();
-print scalar keys %$g2p_pmids, "\n";
 
 my $text_mining_disease_adaptor = $registry->get_adaptor('human', 'gene2phenotype', 'TextMiningDisease');
 my $publication_adaptor = $registry->get_adaptor('human', 'gene2phenotype', 'Publication');
 
-#foreach my $pmid (keys %$g2p_pmids) {
-  my $publication = $publication_adaptor->fetch_by_PMID(16816024);
+foreach my $pmid (keys %$g2p_pmids) {
+  my $publication = $publication_adaptor->fetch_by_PMID($pmid);
   print $publication->title, "\n";
   $text_mining_disease_adaptor->store_all_by_Publication_REST($publication);
-
-
-  die;
-#}
-
+}
 
 
 sub get_pmids_linked_to_GFDs {
