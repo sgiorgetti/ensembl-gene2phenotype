012295f52cc85279e4a8257be891ff136d13a522
Anja Thormann <anja@ebi.ac.uk>
2019-11-21 13:51:05 +0000

    restructure

--- scripts/print_lgmde_threads.pl
+++ scripts/print_lgmde_threads.pl
@@ -5,7 +5,7 @@ use Bio::EnsEMBL::Registry;
 use Data::Dumper;
 use FileHandle;
 
-my $registry_file = '/Users/anja/Documents/G2P/ensembl.registry.oct2019';
+my $registry_file = '/Users/anja/Documents/G2P/ensembl.registry.nov2019';
 my $registry = 'Bio::EnsEMBL::Registry';
 $registry->load_all($registry_file);
 my $dbh = $registry->get_DBAdaptor('human', 'gene2phenotype')->dbc->db_handle;
@@ -19,107 +19,60 @@ my $panels = $panel_adaptor->fetch_all;
 # locus-genotype-mechanism-disease-evidence
 # print_entries_with_more_than_one_action();
 
-#print_phenotype_overlap();
+#create_lgmde_threads();
 
-sub print_phenotype_overlap {
-  my $threads = {};
-
-  foreach my $panel (@$panels) {
-    my $name = $panel->name;
-    next if ($name ne 'DD');
-    my $gfds = $gfda->fetch_all_by_panel($name);
-    foreach my $gfd (sort { $a->get_GenomicFeature->gene_symbol cmp $b->get_GenomicFeature->gene_symbol } @$gfds) {
-      my $gene = $gfd->get_GenomicFeature->gene_symbol;
-      my $disease = $gfd->get_Disease->name;
-      my $actions = $gfd->get_all_GenomicFeatureDiseaseActions; 
-      if (scalar @$actions == 1) {
-        my $action = $actions->[0];
-        my $allelic_requirement = $action->allelic_requirement || 'NA'; 
-        my $mutation_consequence = $action->mutation_consequence || 'NA';
-        my $key = join(' ', $gene, $allelic_requirement, $mutation_consequence);
-        $threads->{$key}->{$name}->{$disease} = $gfd->dbID;
-      }
-    }
-  }
-  print scalar keys %$threads, "\n";
-  my $unique_threads = 0;
-  my $duplicated_threads = 0;
-  foreach my $thread (sort keys %$threads) {
-    if (scalar keys %{$threads->{$thread}} > 1) {
-      $duplicated_threads++;
-      foreach my $panel (sort keys %{$threads->{$thread}}) {
-        foreach my $disease (sort keys %{$threads->{$thread}->{$panel}}) {
-
-        }
-      }
-    } else {
-      $unique_threads++;
-    }
-  }
-
-  print "Duplicated threads $duplicated_threads\n";
-  print "Unique threads $unique_threads\n";
-
-
-
+foreach my $panel (sort {$a->name cmp $b->name} @$panels) {
+  my $gfds = $gfda->fetch_all_by_panel($panel->name);
+  my $gene_disease_threads = scalar @$gfds;
+  my ($unique_lgmd_threads, $duplicated_lgmd_threads, $more_than_one_action, $no_gm) = get_lgmd_threads($gfds);  
+  my $total_lgmd_threads = $unique_lgmd_threads + $duplicated_lgmd_threads;
+  print $panel->name, ' ', "$gene_disease_threads $total_lgmd_threads ($duplicated_lgmd_threads), $more_than_one_action, $no_gm\n";
 }
 
+my $all_gfds =  $gfda->fetch_all_by_panel('ALL');
+my $gene_disease_threads = scalar @$all_gfds;
+my ($unique_lgmd_threads, $duplicated_lgmd_threads, $more_than_one_action, $no_gm) = get_lgmd_threads($all_gfds);  
+my $total_lgmd_threads = $unique_lgmd_threads + $duplicated_lgmd_threads;
+print "ALL $gene_disease_threads $total_lgmd_threads ($duplicated_lgmd_threads), $more_than_one_action, $no_gm\n";
 
-create_lgmde_threads();
 
-sub create_lgmde_threads {
+
+sub get_lgmd_threads {
+  my $gfds = shift;
   my $threads = {};
   my $with_phenotype = 1;
-  my $fh = FileHandle->new("/Users/anja/Documents/G2P/lgmd/create_lgmde_threads_Skin", 'w');
-  my $gene_disease_to_gfd = {};
-  foreach my $panel (@$panels) {
-    my $name = $panel->name;
-    next if ($name ne 'Skin');
-    my $gfds = $gfda->fetch_all_by_panel($name);
-    foreach my $gfd (sort { $a->get_GenomicFeature->gene_symbol cmp $b->get_GenomicFeature->gene_symbol } @$gfds) {
-      my $gene = $gfd->get_GenomicFeature->gene_symbol;
-      my $disease = $gfd->get_Disease->name;
-      my $actions = $gfd->get_all_GenomicFeatureDiseaseActions; 
-      $gene_disease_to_gfd->{"$gene\_$disease"} = $gfd;
-      if (scalar @$actions == 1) {
-        my $action = $actions->[0];
-        my $allelic_requirement = $action->allelic_requirement || 'NA'; 
-        my $mutation_consequence = $action->mutation_consequence || 'NA';
-        my $key = join(' ', $gene, $allelic_requirement, $mutation_consequence);
-        $gene_disease_to_gfd->{"$key\_$disease"} = $gfd;
-        $threads->{$key}->{$disease}++;
-      } else {
-        print "More than one action\n";
-      }
+  my $more_than_one_action = 0;
+  my $no_gm = 0;
+
+  foreach my $gfd (sort { $a->get_GenomicFeature->gene_symbol cmp $b->get_GenomicFeature->gene_symbol } @$gfds) {
+    my $gene = $gfd->get_GenomicFeature->gene_symbol;
+    my $disease = $gfd->get_Disease->name;
+    my $actions = $gfd->get_all_GenomicFeatureDiseaseActions; 
+    if (scalar @$actions > 1) {
+      $more_than_one_action++;
     }
+    foreach my $action (@$actions) { 
+      my $allelic_requirement = $action->allelic_requirement || 'NA'; 
+      my $mutation_consequence = $action->mutation_consequence || 'NA';
+      my $key = join(' ', $gene, $allelic_requirement, $mutation_consequence);
+      $threads->{$key}->{$disease}++;
+    }
+    if (scalar @$actions == 0) {
+      $no_gm++;
+      $threads->{"$gene\_$disease"}->{$disease}++;
+    }
+
   }
-#  print $fh Dumper $threads;
-  print scalar keys %$threads, "\n";
   my $unique_threads = 0;
   my $duplicated_threads = 0;
   foreach my $thread (sort keys %$threads) {
     if (scalar keys %{$threads->{$thread}} > 1) {
       $duplicated_threads++;
-      print $fh ">>$thread\n";
-      foreach my $disease (sort keys %{$threads->{$thread}}) {
-          if ($with_phenotype) {
-            my $gfd = $gene_disease_to_gfd->{"$thread\_$disease"}; 
-            my $phenotypes = join(', ', sort map {$_->get_Phenotype->name} @{$gfd->get_all_GFDPhenotypes}); 
-            print $fh "        >>$disease:  $phenotypes\n\n";
-          } else {
-            print $fh "        $disease\n"; 
-          }
-      }
-      print $fh "\n";
     } else {
       $unique_threads++;
     }
   }
-
-  $fh->close;
-  print "Duplicated threads $duplicated_threads\n";
-  print "Unique threads $unique_threads\n";
-
+  return ($unique_threads, $duplicated_threads, $more_than_one_action, $no_gm);
 }
 
 
