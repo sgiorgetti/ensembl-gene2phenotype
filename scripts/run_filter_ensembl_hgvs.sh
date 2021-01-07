01054aa1b5ad04105c32fb8dd0c580edee5b4658
Anja Thormann <anja@ebi.ac.uk>
2021-01-07 13:23:09 +0000

    deleted

--- scripts/run_filter_ensembl_hgvs.sh
+++
@@ -1,1 +0,0 @@
-bsub -J filter -o /hps/nobackup/production/ensembl/anja/G2P/text_mining/filter_hgvs.out -e /hps/nobackup/production/ensembl/anja/G2P/text_mining/filter_hgvs.err perl filter_ensembl_hgvs.pl 
