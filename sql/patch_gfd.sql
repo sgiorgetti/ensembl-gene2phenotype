5581746205217f96abfa7a5ffb26fdcf1acb707b
at7 <anja@ebi.ac.uk>
2021-03-08 13:19:39 +0000

    update gfd

--- sql/patch_gfd.sql
+++ sql/patch_gfd.sql
@@ -11,7 +11,8 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
-ALTER TABLE genomic_feature_disease ADD COLUMN disease_name varchar(255) DEFAULT NULL AFTER disease_id;
-ALTER TABLE genomic_feature_disease ADD COLUMN disease_mim int(10) unsigned DEFAULT NULL AFTER disease_name;
-ALTER TABLE genomic_feature_disease ADD COLUMN allelic_requirement_attrib set('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20') DEFAULT NULL AFTER disease_mim;
+
+ALTER TABLE genomic_feature_disease ADD COLUMN allelic_requirement_attrib set('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20') DEFAULT NULL AFTER disease_id;
 ALTER TABLE genomic_feature_disease ADD COLUMN mutation_consequence_attrib set('21', '22', '23', '24', '25', '26', '27', '28', '29', '30') DEFAULT NULL AFTER allelic_requirement_attrib;
+
+UPDATE genomic_feature_disease gfd, genomic_feature_disease_action gfda SET gfd.allelic_requirement_attrib = gfda.allelic_requirement_attrib, gfd.mutation_consequence_attrib = gfda.mutation_consequence_attrib WHERE gfda.genomic_feature_disease_id = gfd.genomic_feature_disease_id;
