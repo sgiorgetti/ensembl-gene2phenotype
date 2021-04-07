12512ca9278a8b5234bf6e9393e3cc80f3ea52ec
at7 <anja@ebi.ac.uk>
2021-04-07 13:18:24 +0100

    update sub

--- modules/Bio/EnsEMBL/G2P/DBSQL/GenomicFeatureDiseaseLogAdaptor.pm
+++ modules/Bio/EnsEMBL/G2P/DBSQL/GenomicFeatureDiseaseLogAdaptor.pm
@@ -40,7 +40,8 @@ sub store {
       genomic_feature_disease_id,
       genomic_feature_id,
       disease_id,
-
+      allelic_requirement_attrib,
+      mutation_consequence_attrib,
       created,
       user_id,
       action
@@ -51,7 +52,8 @@ sub store {
     $gfd_log->{genomic_feature_disease_id},
     $gfd_log->{genomic_feature_id},
     $gfd_log->{disease_id},
-
+    $gfd_log->{allelic_requirement_attrib},
+    $gfd_log->{mutation_consequence_attrib},
     $gfd_log->user_id,
     $gfd_log->action,
   );
@@ -79,17 +81,13 @@ sub fetch_all_by_GenomicFeatureDisease {
   return $self->generic_fetch($constraint);
 }
 
-sub fetch_latest_updates {
+sub fetch_all_by_most_recent {
   my $self = shift;
-  my $panel = shift;
-  my $limit = shift; # 10
-  my $is_visible_only = shift;
-  my $aa = $self->db->get_AttributeAdaptor;
-  my $panel_attrib = $aa->attrib_id_for_type_value('g2p_panel', $panel);
-  my $constraint = "gfdl.panel_attrib='$panel_attrib' AND gfdl.action='create'";
-  if ($is_visible_only) {
-    $constraint .= " AND gfd.is_visible = 1";
-  }
+  my $limit = shift;
+  my $action = shift;
+  $limit ||= 10;
+  $action ||= 'create';
+  my $constraint = "gfdl.action='$action'";
   $constraint .= " ORDER BY created DESC limit $limit";
   return $self->generic_fetch($constraint);
 }
@@ -101,11 +99,11 @@ sub _columns {
     'gfdl.genomic_feature_disease_id',
     'gfdl.genomic_feature_id',
     'gfdl.disease_id',
+    'gfdl.allelic_requirement_attrib',
+    'gfdl.mutation_consequence_attrib',
     'gfdl.created',
     'gfdl.user_id',
     'gfdl.action',
-    'gf.gene_symbol',
-    'd.name as disease_name'
   );
   return @cols;
 }
@@ -115,8 +113,6 @@ sub _tables {
   my @tables = (
     ['genomic_feature_disease_log', 'gfdl'],
     ['genomic_feature_disease', 'gfd'],
-    ['genomic_feature', 'gf'],
-    ['disease', 'd']
   );
   return @tables;
 }
@@ -126,8 +122,6 @@ sub _left_join {
 
   my @left_join = (
     ['genomic_feature_disease', 'gfdl.genomic_feature_disease_id = gfd.genomic_feature_disease_id'],
-    ['genomic_feature', 'gfdl.genomic_feature_id = gf.genomic_feature_id'],
-    ['disease', 'gfdl.disease_id = d.disease_id'],
   );
   return @left_join;
 }
@@ -140,27 +134,25 @@ sub _objs_from_sth {
     $genomic_feature_disease_id,
     $genomic_feature_id,
     $disease_id,
+    $allelic_requirement_attrib,
+    $mutation_consequence_attrib,
     $created,
     $user_id,
     $action,
-    $gene_symbol,
-    $disease_name
   );
   $sth->bind_columns(\(
     $genomic_feature_disease_log_id,
     $genomic_feature_disease_id,
     $genomic_feature_id,
     $disease_id,
+    $allelic_requirement_attrib,
+    $mutation_consequence_attrib,
     $created,
     $user_id,
     $action,
-    $gene_symbol,
-    $disease_name
   ));
 
   my @objs;
-
-  my $attribute_adaptor = $self->db->get_AttributeAdaptor;
 
   while ($sth->fetch()) {
     my $confidence_category = undef; 
@@ -169,11 +161,11 @@ sub _objs_from_sth {
       -genomic_feature_disease_id => $genomic_feature_disease_id,
       -genomic_feature_id => $genomic_feature_id,
       -disease_id => $disease_id,
+      -allelic_requirement_attrib => $allelic_requirement_attrib,
+      -mutation_consequence_attrib => $mutation_consequence_attrib,
       -created => $created,
       -user_id => $user_id,
       -action => $action,
-      -gene_symbol => $gene_symbol,
-      -disease_name => $disease_name,
       -adaptor => $self,
     );
     push(@objs, $obj);
