67f93b05bef8e37b6ca3778a47bc1faeed49a8eb
at7 <anja@ebi.ac.uk>
2021-04-15 19:54:12 +0100

    improve error message, add new sub

--- modules/Bio/EnsEMBL/G2P/DBSQL/AttributeAdaptor.pm
+++ modules/Bio/EnsEMBL/G2P/DBSQL/AttributeAdaptor.pm
@@ -28,13 +28,12 @@ sub get_attrib {
   my $self = shift;
   my $type = shift;
   my $value = shift;
-
   if ($type eq 'allelic_requirement') {
     my @ids = ();
     foreach my $v (split(',', $value)) {
       my $id = $self->attrib_id_for_type_value($type, $v);
       if (!$id) {
-        die "Could not get attrib for value: $v\n";
+        die "Could not get attrib for value: $v and type: $type\n";
       }
       push @ids, $self->attrib_id_for_type_value($type, $v);
     }        
@@ -42,7 +41,7 @@ sub get_attrib {
   } else {
     my $attrib = $self->attrib_id_for_type_value($type, $value);
     if (!$attrib) {
-      die "Could not get attrib for value: $value\n";
+      die "Could not get attrib for value: $value and type: $type\n";
     }
     return $attrib;
   }
@@ -52,13 +51,12 @@ sub get_value {
   my $self = shift;
   my $type = shift;
   my $attrib = shift;
-
   if ($type eq 'allelic_requirement') {
     my @values = ();
     foreach my $id (split(',', $attrib)) {
       my $value =  $self->attrib_value_for_type_id($type, $id);
       if (!$value) {
-        die "Could not get value for attrib: $id\n";
+        die "Could not get value for attrib: $id and type: $type\n";
       }
       push @values, $value;
     }
@@ -66,7 +64,7 @@ sub get_value {
   } else {
     my $value = $self->attrib_value_for_type_id($type, $attrib);
     if (!$value) {
-      die "Could not get value for attrib: $attrib\n";
+      die "Could not get value for attrib: $attrib and type: $type\n";
     }
     return $value;
   }
@@ -117,7 +115,7 @@ sub attrib_value_for_type_id {
   return $self->{attrib_values}->{$type}->{$attrib_id};
 }
 
-sub get_attribs_by_type {
+sub get_values_by_type {
   my ($self, $attrib_type_code) = @_;
   my $attribs = {};
   my $sql = qq{
@@ -137,6 +135,28 @@ sub get_attribs_by_type {
   $sth->finish();
   return $attribs;
 }
+
+sub get_attribs_by_type {
+  my ($self, $attrib_type_code) = @_;
+  my $attribs = {};
+  my $sql = qq{
+    SELECT  a.attrib_id, a.value
+    FROM    attrib a, attrib_type t
+    WHERE   a.attrib_type_id = t.attrib_type_id
+    AND     t.code = ?;
+  };
+
+  my $dbh = $self->dbc->db_handle;
+  my $sth = $dbh->prepare($sql);
+  $sth->execute($attrib_type_code);
+
+  while (my ($attrib_id, $value) = $sth->fetchrow_array) {
+    $attribs->{$attrib_id} = $value;
+  }
+  $sth->finish();
+  return $attribs;
+}
+
 
 
 
