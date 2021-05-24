0298be17fa3bebdf82502a9a0d2aca6ecbaaf295
at7 <anja@ebi.ac.uk>
2021-05-24 16:57:57 +0100

    add meta table

--- modules/t/test-genome-DBs/homo_sapiens/gene2phenotype/table.sql
+++ modules/t/test-genome-DBs/homo_sapiens/gene2phenotype/table.sql
@@ -1,3 +1,13 @@
+CREATE TABLE `meta` (
+  `meta_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `species_id` int(10) unsigned DEFAULT '1',
+  `meta_key` varchar(40) NOT NULL,
+  `meta_value` varchar(255) NOT NULL,
+  PRIMARY KEY (`meta_id`),
+  UNIQUE KEY `species_key_value_idx` (`species_id`,`meta_key`,`meta_value`),
+  KEY `species_value_idx` (`species_id`,`meta_value`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
 CREATE TABLE `GFD_comment_deleted` (
   `GFD_comment_deleted_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `genomic_feature_disease_id` int(10) unsigned NOT NULL,
