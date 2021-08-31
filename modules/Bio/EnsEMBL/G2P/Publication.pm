e9031a31901c1a043821998e9da202b019da627c
at7 <anja@ebi.ac.uk>
2021-08-31 11:33:27 +0100

    remove legacy code

--- modules/Bio/EnsEMBL/G2P/Publication.pm
+++ modules/Bio/EnsEMBL/G2P/Publication.pm
@@ -73,9 +73,4 @@ sub source {
   return $self->{source};
 }
 
-sub update_text_minig_results {
-  my $self = shift;
-  my $text_mining_disease_adaptor = $self->{adaptor}->db->get_TextMiningDiseaseAdaptor;
-}
-
 1;
