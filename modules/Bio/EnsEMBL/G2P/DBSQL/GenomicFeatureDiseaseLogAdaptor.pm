362f4a952daa31f007333cb68f3f2378efbd00df
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2022-03-24 16:23:39 +0000

    Changes

--- modules/Bio/EnsEMBL/G2P/DBSQL/GenomicFeatureDiseaseLogAdaptor.pm
+++ modules/Bio/EnsEMBL/G2P/DBSQL/GenomicFeatureDiseaseLogAdaptor.pm
@@ -64,7 +64,7 @@ sub store {
       created,
       user_id,
       action
-    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ? CURRENT_TIMESTAMP, ?, ?)
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP, ?, ?)
   });
 
   $sth->execute(
