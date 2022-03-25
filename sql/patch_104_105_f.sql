3bd8e7d72125208fdd5960a3aa530a3a87d012ac
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2022-03-25 11:08:24 +0000

    Adding the changes to the sql

--- sql/patch_104_105_f.sql
+++ sql/patch_104_105_f.sql
@@ -14,12 +14,23 @@
 -- limitations under the License.
 
 
-INSERT into attrib_type(code, name, description) VALUES('variant_consequence', 'Variant consequence', 'Variant consequence using certain SO terms');
+INSERT IGNORE into attrib_type(attrib_type_id, code, name, description) VALUES(16, 'variant_consequence', 'Variant consequence', 'Variant consequence using certain terms');
 
-INSERT into attrib(attrib_id, attrib_type_id, value) VALUES (100, 16, 'splice_region_variant'), (101, 16, 'splice_acceptor_variant'), (102, 16, 'splice_donor_variant'),
-(103, 16, 'start_lost'), (104, 16, 'frameshift_variant'), (105, 16, 'stop_gained'), (106, 16, 'stop_lost'), (107, 16, 'missense_variant'), (108, 16, 'inframe_deletion'),
-(109, 16, '5_prime_UTR_variant'), (110, 16, '3_prime_UTR_variant'), (111, 16, 'synonymous_variant'), (112, 16, 'intron_variant'), 
-(113, 16, 'regulatory_region_variant'), (114, 16, 'intergenic_variant');
+INSERT IGNORE into attrib(attrib_id, attrib_type_id, value) VALUES (100, 16, 'splice_region_variant');
+INSERT IGNORE into attrib(attrib_id, attrib_type_id, value) VALUES (101, 16, 'splice_acceptor_variant');
+INSERT IGNORE into attrib(attrib_id, attrib_type_id, value) VALUES (102, 16, 'splice_donor_variant');
+INSERT IGNORE into attrib(attrib_id, attrib_type_id, value) VALUES (103, 16, 'start_lost');
+INSERT IGNORE into attrib(attrib_id, attrib_type_id, value) VALUES (104, 16, 'frameshift_variant'); 
+INSERT IGNORE into attrib(attrib_id, attrib_type_id, value) VALUES (105, 16, 'stop_gained');
+INSERT IGNORE into attrib(attrib_id, attrib_type_id, value) VALUES (106, 16, 'stop_lost');
+INSERT IGNORE into attrib(attrib_id, attrib_type_id, value) VALUES (107, 16, 'missense_variant'); 
+INSERT IGNORE into attrib(attrib_id, attrib_type_id, value) VALUES (108, 16, 'inframe_deletion');
+INSERT IGNORE into attrib(attrib_id, attrib_type_id, value) VALUES (109, 16, '5_prime_UTR_variant'); 
+INSERT IGNORE into attrib(attrib_id, attrib_type_id, value) VALUES (110, 16, '3_prime_UTR_variant'); 
+INSERT IGNORE into attrib(attrib_id, attrib_type_id, value) VALUES (111, 16, 'synonymous_variant');
+INSERT IGNORE into attrib(attrib_id, attrib_type_id, value) VALUES (112, 16, 'intron_variant');
+INSERT IGNORE into attrib(attrib_id, attrib_type_id, value) VALUES (113, 16, 'regulatory_region_variant');
+INSERT IGNORE into attrib(attrib_id, attrib_type_id, value) VALUES (114, 16, 'intergenic_variant');
 
 ALTER TABLE genomic_feature_disease ADD COLUMN variant_consequence_attrib set('100', '101', '102', '103', '104', '105', '106', '107', '108', '109', '110', '111', '112', '113', '114') DEFAULT NULL AFTER mutational_consequence_flag_attrib;
 
