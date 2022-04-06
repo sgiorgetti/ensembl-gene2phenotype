ff070b9510ca010faf32ceaf85e8c85dbdce5374
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2022-04-06 10:25:02 +0100

    Changes to the test/table.sql

--- modules/t/test-genome-DBs/homo_sapiens/gene2phenotype/table.sql
+++ modules/t/test-genome-DBs/homo_sapiens/gene2phenotype/table.sql
@@ -377,6 +377,6 @@ CREATE TABLE disease_ontology_mapping (
   `ontology_accession_id` INT(10) NOT NULL, 
   `mapped_by_attrib` set('437', '438', '439', '440', '441', '442', '443', '444') DEFAULT NULL,
   PRIMARY KEY (`disease_ontology_mapping_id`),
-  KEY `ontology_term_idx` (`ontology_accession_id`)
+  KEY `ontology_term_idx` (`ontology_accession_id`),
   KEY `disease_idx` (`disease_id`)
 ) ENGINE=INNODB; 
