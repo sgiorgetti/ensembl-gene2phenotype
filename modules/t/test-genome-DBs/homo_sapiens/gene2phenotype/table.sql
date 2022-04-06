29eb8bf761e9738ee28a79ce1f37393c3c50ca84
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2022-04-06 10:17:42 +0100

    Changes to the test/table and table.sql

--- modules/t/test-genome-DBs/homo_sapiens/gene2phenotype/table.sql
+++ modules/t/test-genome-DBs/homo_sapiens/gene2phenotype/table.sql
@@ -364,3 +364,19 @@ CREATE TABLE `user` (
   UNIQUE KEY `email_idx` (`email`)
 ) ENGINE=InnoDB  ;
 
+CREATE TABLE `ontology_term` (
+  `ontology_accession_id` int(10) NOT NULL,
+  `ontology_accession` VARCHAR(255) DEFAULT NULL,
+  `description` VARCHAR(255) DEFAULT NULL, 
+  PRIMARY KEY (`ontology_accession_id`)
+) ENGINE=INNODB; 
+
+CREATE TABLE disease_ontology_mapping (
+  `disease_ontology_mapping_id` int(10) NOT NULL, 
+  `disease_id` INT(10) NOT NULL,
+  `ontology_accession_id` INT(10) NOT NULL, 
+  `mapped_by_attrib` set('437', '438', '439', '440', '441', '442', '443', '444') DEFAULT NULL,
+  PRIMARY KEY (`disease_ontology_mapping_id`),
+  KEY `ontology_term_idx` (`ontology_accession_id`)
+  KEY `disease_idx` (`disease_id`)
+) ENGINE=INNODB; 
